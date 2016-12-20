b8a18613f7c77f1c85d55e50bdcc7da8dbf54f2f
a-einstein <aeinstein@xs4all.nl>
2016-12-20 22:33:56 +0100

    Getting ProductsView showing data. Adding direct simple styling.

--- Portable/RCS.PortableShop/Main/MainWindow.cs
+++ Portable/RCS.PortableShop/Main/MainWindow.cs
@@ -8,8 +8,11 @@ namespace RCS.PortableShop.Main
     {
         public MainWindow()
         {
-            var view = new ProductsView();
-            view.ViewModel = new ProductsViewModel();
+            var viewModel = new ProductsViewModel();
+            var view = new ProductsView(viewModel);
+
+            // TODO Does not work. Get rid of large margin/padding.
+            Padding = 0;
 
             Content = view;
         }
