4e96033b4819207408f1f86738097359f0a36bc6
a-einstein <aeinstein@xs4all.nl>
2016-12-27 22:12:17 +0100

    Made ViewCell openable instead of by Button. Created custom ContentButton.

---
+++ Portable/RCS.PortableShop.Common/Controls/ContentButton.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace RCS.PortableShop.Common.Controls
+{
+    public partial class ContentButton : ContentView
+    {
+        public ContentButton()
+        {
+            InitializeComponent();
+        }
+
+        public event EventHandler TappedHandler;
+
+        public static readonly BindableProperty CommandProperty =
+            BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(ContentButton));
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public static readonly BindableProperty CommandParameterProperty =
+            BindableProperty.Create(nameof(CommandParameter), typeof(object), typeof(ContentButton));
+
+        public object CommandParameter
+        {
+            get { return (object)GetValue(CommandParameterProperty); }
+            set { SetValue(CommandParameterProperty, value); }
+        }
+
+        private void TapGestureRecognizer_OnTapped(object sender, EventArgs e)
+        {
+            TappedHandler?.Invoke(this, e);
+        }
+    }
+}
