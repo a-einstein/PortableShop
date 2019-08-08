6af2be57a56dbf35e8aa9b2c3d8a937709a10977
a-einstein <aeinstein@xs4all.nl>
2019-08-08 19:43:04 +0200

    Syncfusion.

--- Platforms/RCS.PortableShop.Droid/Resources/Resource.Designer.cs
+++ Platforms/RCS.PortableShop.Droid/Resources/Resource.Designer.cs
@@ -26,9 +26,12 @@ namespace RCS.PortableShop.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Syncfusion.XForms.Android.Core.Resource.Drawable.input_layout_drop_down_icon = global::RCS.PortableShop.Droid.Resource.Drawable.input_layout_drop_down_icon;
 			global::Syncfusion.XForms.Android.Core.Resource.Drawable.inputlayout_togglecollapseicon = global::RCS.PortableShop.Droid.Resource.Drawable.inputlayout_togglecollapseicon;
 			global::Syncfusion.XForms.Android.Core.Resource.Drawable.inputlayout_togglevisibleicon = global::RCS.PortableShop.Droid.Resource.Drawable.inputlayout_togglevisibleicon;
 			global::Com.Syncfusion.Numericupdown.Resource.String.library_name = global::RCS.PortableShop.Droid.Resource.String.library_name;
+			global::Com.Syncfusion.Numericupdown.Resource.String.sfnumericupdown_decrement_button_accessibility_label = global::RCS.PortableShop.Droid.Resource.String.sfnumericupdown_decrement_button_accessibility_label;
+			global::Com.Syncfusion.Numericupdown.Resource.String.sfnumericupdown_increment_button_accessibility_label = global::RCS.PortableShop.Droid.Resource.String.sfnumericupdown_increment_button_accessibility_label;
 			global::Xamarin.Forms.Platform.Android.Resource.Animation.abc_fade_in = global::RCS.PortableShop.Droid.Resource.Animation.abc_fade_in;
 			global::Xamarin.Forms.Platform.Android.Resource.Animation.abc_fade_out = global::RCS.PortableShop.Droid.Resource.Animation.abc_fade_out;
 			global::Xamarin.Forms.Platform.Android.Resource.Animation.abc_grow_fade_in_from_bottom = global::RCS.PortableShop.Droid.Resource.Animation.abc_grow_fade_in_from_bottom;
@@ -5599,26 +5602,26 @@ namespace RCS.PortableShop.Droid
 			// aapt resource value: 0x7f020057
 			public const int avd_hide_password = 2130837591;
 			
-			// aapt resource value: 0x7f02013c
-			public const int avd_hide_password_1 = 2130837820;
-			
 			// aapt resource value: 0x7f02013d
-			public const int avd_hide_password_2 = 2130837821;
+			public const int avd_hide_password_1 = 2130837821;
 			
 			// aapt resource value: 0x7f02013e
-			public const int avd_hide_password_3 = 2130837822;
+			public const int avd_hide_password_2 = 2130837822;
+			
+			// aapt resource value: 0x7f02013f
+			public const int avd_hide_password_3 = 2130837823;
 			
 			// aapt resource value: 0x7f020058
 			public const int avd_show_password = 2130837592;
 			
-			// aapt resource value: 0x7f02013f
-			public const int avd_show_password_1 = 2130837823;
-			
 			// aapt resource value: 0x7f020140
-			public const int avd_show_password_2 = 2130837824;
+			public const int avd_show_password_1 = 2130837824;
 			
 			// aapt resource value: 0x7f020141
-			public const int avd_show_password_3 = 2130837825;
+			public const int avd_show_password_2 = 2130837825;
+			
+			// aapt resource value: 0x7f020142
+			public const int avd_show_password_3 = 2130837826;
 			
 			// aapt resource value: 0x7f020059
 			public const int design_bottom_navigation_item_background = 2130837593;
@@ -6179,127 +6182,130 @@ namespace RCS.PortableShop.Droid
 			public const int ic_vol_type_tv_light = 2130837778;
 			
 			// aapt resource value: 0x7f020113
-			public const int inputlayout_togglecollapseicon = 2130837779;
+			public const int input_layout_drop_down_icon = 2130837779;
 			
 			// aapt resource value: 0x7f020114
-			public const int inputlayout_togglevisibleicon = 2130837780;
+			public const int inputlayout_togglecollapseicon = 2130837780;
 			
 			// aapt resource value: 0x7f020115
-			public const int mr_button_connected_dark = 2130837781;
+			public const int inputlayout_togglevisibleicon = 2130837781;
 			
 			// aapt resource value: 0x7f020116
-			public const int mr_button_connected_light = 2130837782;
+			public const int mr_button_connected_dark = 2130837782;
 			
 			// aapt resource value: 0x7f020117
-			public const int mr_button_connecting_dark = 2130837783;
+			public const int mr_button_connected_light = 2130837783;
 			
 			// aapt resource value: 0x7f020118
-			public const int mr_button_connecting_light = 2130837784;
+			public const int mr_button_connecting_dark = 2130837784;
 			
 			// aapt resource value: 0x7f020119
-			public const int mr_button_dark = 2130837785;
+			public const int mr_button_connecting_light = 2130837785;
 			
 			// aapt resource value: 0x7f02011a
-			public const int mr_button_light = 2130837786;
+			public const int mr_button_dark = 2130837786;
 			
 			// aapt resource value: 0x7f02011b
-			public const int mr_dialog_close_dark = 2130837787;
+			public const int mr_button_light = 2130837787;
 			
 			// aapt resource value: 0x7f02011c
-			public const int mr_dialog_close_light = 2130837788;
+			public const int mr_dialog_close_dark = 2130837788;
 			
 			// aapt resource value: 0x7f02011d
-			public const int mr_dialog_material_background_dark = 2130837789;
+			public const int mr_dialog_close_light = 2130837789;
 			
 			// aapt resource value: 0x7f02011e
-			public const int mr_dialog_material_background_light = 2130837790;
+			public const int mr_dialog_material_background_dark = 2130837790;
 			
 			// aapt resource value: 0x7f02011f
-			public const int mr_group_collapse = 2130837791;
+			public const int mr_dialog_material_background_light = 2130837791;
 			
 			// aapt resource value: 0x7f020120
-			public const int mr_group_expand = 2130837792;
+			public const int mr_group_collapse = 2130837792;
 			
 			// aapt resource value: 0x7f020121
-			public const int mr_media_pause_dark = 2130837793;
+			public const int mr_group_expand = 2130837793;
 			
 			// aapt resource value: 0x7f020122
-			public const int mr_media_pause_light = 2130837794;
+			public const int mr_media_pause_dark = 2130837794;
 			
 			// aapt resource value: 0x7f020123
-			public const int mr_media_play_dark = 2130837795;
+			public const int mr_media_pause_light = 2130837795;
 			
 			// aapt resource value: 0x7f020124
-			public const int mr_media_play_light = 2130837796;
+			public const int mr_media_play_dark = 2130837796;
 			
 			// aapt resource value: 0x7f020125
-			public const int mr_media_stop_dark = 2130837797;
+			public const int mr_media_play_light = 2130837797;
 			
 			// aapt resource value: 0x7f020126
-			public const int mr_media_stop_light = 2130837798;
+			public const int mr_media_stop_dark = 2130837798;
 			
 			// aapt resource value: 0x7f020127
-			public const int mr_vol_type_audiotrack_dark = 2130837799;
+			public const int mr_media_stop_light = 2130837799;
 			
 			// aapt resource value: 0x7f020128
-			public const int mr_vol_type_audiotrack_light = 2130837800;
+			public const int mr_vol_type_audiotrack_dark = 2130837800;
 			
 			// aapt resource value: 0x7f020129
-			public const int mtrl_snackbar_background = 2130837801;
+			public const int mr_vol_type_audiotrack_light = 2130837801;
 			
 			// aapt resource value: 0x7f02012a
-			public const int mtrl_tabs_default_indicator = 2130837802;
+			public const int mtrl_snackbar_background = 2130837802;
 			
 			// aapt resource value: 0x7f02012b
-			public const int navigation_empty_icon = 2130837803;
+			public const int mtrl_tabs_default_indicator = 2130837803;
 			
 			// aapt resource value: 0x7f02012c
-			public const int notification_action_background = 2130837804;
+			public const int navigation_empty_icon = 2130837804;
 			
 			// aapt resource value: 0x7f02012d
-			public const int notification_bg = 2130837805;
+			public const int notification_action_background = 2130837805;
 			
 			// aapt resource value: 0x7f02012e
-			public const int notification_bg_low = 2130837806;
+			public const int notification_bg = 2130837806;
 			
 			// aapt resource value: 0x7f02012f
-			public const int notification_bg_low_normal = 2130837807;
+			public const int notification_bg_low = 2130837807;
 			
 			// aapt resource value: 0x7f020130
-			public const int notification_bg_low_pressed = 2130837808;
+			public const int notification_bg_low_normal = 2130837808;
 			
 			// aapt resource value: 0x7f020131
-			public const int notification_bg_normal = 2130837809;
+			public const int notification_bg_low_pressed = 2130837809;
 			
 			// aapt resource value: 0x7f020132
-			public const int notification_bg_normal_pressed = 2130837810;
+			public const int notification_bg_normal = 2130837810;
 			
 			// aapt resource value: 0x7f020133
-			public const int notification_icon_background = 2130837811;
-			
-			// aapt resource value: 0x7f02013a
-			public const int notification_template_icon_bg = 2130837818;
-			
-			// aapt resource value: 0x7f02013b
-			public const int notification_template_icon_low_bg = 2130837819;
+			public const int notification_bg_normal_pressed = 2130837811;
 			
 			// aapt resource value: 0x7f020134
-			public const int notification_tile_bg = 2130837812;
+			public const int notification_icon_background = 2130837812;
+			
+			// aapt resource value: 0x7f02013b
+			public const int notification_template_icon_bg = 2130837819;
+			
+			// aapt resource value: 0x7f02013c
+			public const int notification_template_icon_low_bg = 2130837820;
 			
 			// aapt resource value: 0x7f020135
-			public const int notify_panel_notification_icon_bg = 2130837813;
+			public const int notification_tile_bg = 2130837813;
 			
 			// aapt resource value: 0x7f020136
-			public const int Refresh = 2130837814;
+			public const int notify_panel_notification_icon_bg = 2130837814;
 			
 			// aapt resource value: 0x7f020137
-			public const int SplashScreen = 2130837815;
+			public const int Refresh = 2130837815;
 			
 			// aapt resource value: 0x7f020138
-			public const int tooltip_frame_dark = 2130837816;
+			public const int SplashScreen = 2130837816;
 			
 			// aapt resource value: 0x7f020139
-			public const int tooltip_frame_light = 2130837817;
+			public const int tooltip_frame_dark = 2130837817;
+			
+			// aapt resource value: 0x7f02013a
+			public const int tooltip_frame_light = 2130837818;
 			
 			static Drawable()
 			{
@@ -7694,6 +7700,12 @@ namespace RCS.PortableShop.Droid
 			
 			// aapt resource value: 0x7f090033
 			public const int search_menu_title = 2131296307;
+			
+			// aapt resource value: 0x7f09004f
+			public const int sfnumericupdown_decrement_button_accessibility_label = 2131296335;
+			
+			// aapt resource value: 0x7f090050
+			public const int sfnumericupdown_increment_button_accessibility_label = 2131296336;
 			
 			// aapt resource value: 0x7f09004d
 			public const int status_bar_notification_info_overflow = 2131296333;
