ddbe2260fafa67aec792491323ffbf70b10dd6a0
a-einstein <aeinstein@xs4all.nl>
2020-01-04 20:17:42 +0100

    Add update button. Add version number.

--- Portable/RCS.PortableShop/ViewModels/MainShellViewModel.cs
+++ Portable/RCS.PortableShop/ViewModels/MainShellViewModel.cs
@@ -8,14 +8,18 @@ namespace RCS.PortableShop.ViewModels
 {
     public class MainShellViewModel : ShoppingViewModel
     {
+        #region Construction
         protected override void SetCommands()
         {
             base.SetCommands();
 
             ShowProductsCommand = new Command(async () => await ShowProducts());
             OpenSupportCommand = new Command(() => OpenSupport());
+            UpdateCommand = new Command(() => Update());
         }
+        #endregion
 
+        #region Products
         public static readonly BindableProperty ShowProductsCommandProperty =
             BindableProperty.Create(nameof(ShowProductsCommand), typeof(ICommand), typeof(MainShellViewModel));
 
@@ -34,12 +38,17 @@ namespace RCS.PortableShop.ViewModels
             Shell.FlyoutIsPresented = false;
             await PopToRoot();
         }
+        #endregion
 
+        #region Cart
         protected override async Task ShowCart()
         {
             Shell.FlyoutIsPresented = false;
             await base.ShowCart();
         }
+        #endregion
+
+        #region Support
 
         public static readonly BindableProperty OpenSupportCommandProperty =
             BindableProperty.Create(nameof(OpenSupportCommand), typeof(ICommand), typeof(MainShellViewModel));
@@ -56,11 +65,43 @@ namespace RCS.PortableShop.ViewModels
 
         protected static void OpenSupport()
         {
-            Shell.FlyoutIsPresented = false;
+            // TODO Make this Configureable.
+            OpenWeb("https://github.com/a-einstein/PortableShop/blob/master/README.md");
+        }
+        #endregion
+
+        #region Update
+        public static readonly BindableProperty UpdateCommandProperty =
+             BindableProperty.Create(nameof(UpdateCommand), typeof(ICommand), typeof(MainShellViewModel));
+
+        public ICommand UpdateCommand
+        {
+            get { return (ICommand)GetValue(UpdateCommandProperty); }
+            set 
+            { 
+                SetValue(UpdateCommandProperty, value); 
+                RaisePropertyChanged(nameof(UpdateCommand));
+            }
+        }
+
+        protected static void Update()
+        {
+            // Note this is only very rough and may fail on the still open application and other technicalities afterwards.
+
+            // TODO Check versions first. Proceed with either reporting no update needed, or consent to close the application. Even better: check automatically at startup.
 
             // TODO Make this Configureable.
-            Browser.OpenAsync(new Uri("https://github.com/a-einstein/PortableShop/blob/master/README.md"));
-
+            OpenWeb("https://rcsadventureworac85.blob.core.windows.net/portableshop-releases/latest/RCS.CyclOne.apk");
         }
+        #endregion
+
+        #region Utilities
+        private static void OpenWeb(string webAddress)
+        {
+            Shell.FlyoutIsPresented = false;
+
+            Browser.OpenAsync(new Uri(webAddress));
+        }
+        #endregion
     }
 }
