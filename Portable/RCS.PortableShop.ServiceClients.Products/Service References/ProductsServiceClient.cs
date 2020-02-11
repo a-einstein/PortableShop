6bae0f1cb8f42257f327f3120305f9626e23fad4
a-einstein <aeinstein@xs4all.nl>
2020-02-11 21:02:26 +0100

    CA2213. Implemented ProductsServiceClient.IDisposable.

---
+++ Portable/RCS.PortableShop.ServiceClients.Products/Service References/ProductsServiceClient.cs
@@ -0,0 +1,39 @@
+﻿using System.ServiceModel;
+
+namespace RCS.PortableShop.ServiceClients.Products.ProductsService
+{
+    // Added this part to implement IDisposable.
+    // Oddly, it wasn't implemented, while it could still build.
+    // Based on https://coding.abel.nu/2012/02/using-and-disposing-of-wcf-clients.
+    public partial class ProductsServiceClient
+    {
+        #region IDisposable
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                try
+                {
+                    if (State != CommunicationState.Faulted)
+                        Close();
+                }
+                finally
+                {
+                    if (State != CommunicationState.Closed)
+                        Abort();
+                }
+            }
+        }
+
+         ~ProductsServiceClient()
+        {
+            Dispose(false);
+        }
+        #endregion
+    }
+}
--- Portable/RCS.PortableShop/Model/ProductsServiceConsumer.cs
+++ Portable/RCS.PortableShop/Model/ProductsServiceConsumer.cs
@@ -145,7 +145,6 @@ namespace RCS.PortableShop.Model
         // Check out the IDisposable documentation for details on the pattern applied here.
         // Note that it can have implications on derived classes too.
 
-        // Has Dispose already been called?
         private bool disposed = false;
 
         public void Dispose()
@@ -159,15 +158,11 @@ namespace RCS.PortableShop.Model
             if (disposed)
                 return;
 
-            // Free managed objects here.
             if (disposing)
             {
-                productsServiceClient?.CloseAsync();
+                productsServiceClient?.Dispose();
                 httpClient?.Dispose();
             }
-
-            // Free unmanaged objects here.
-            { }
 
             disposed = true;
         }
