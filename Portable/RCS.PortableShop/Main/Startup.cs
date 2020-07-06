28dcd8d34d5c6317f7f96d2d523c4dfd56f3c30b
a-einstein <aeinstein@xs4all.nl>
2020-07-06 22:26:26 +0200

    Handled some platform dependent deployment issues.

--- Portable/RCS.PortableShop/Main/Startup.cs
+++ Portable/RCS.PortableShop/Main/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Hosting;
 using RCS.PortableShop.Model;
 using RCS.PortableShop.ServiceClients.Products.Wrappers;
 using System;
+using Xamarin.Essentials;
 
 namespace RCS.PortableShop.Main
 {
@@ -13,36 +14,73 @@ namespace RCS.PortableShop.Main
 
         public static void Init()
         {
-            // Had to use this way of instantiation as new HostBuilder() gave exceptions.
+            /*
+             * Note other peculiarities for deployment.
+             * - Android can't handle too long development paths, like described here.
+             *   https://stackoverflow.com/questions/57744837/could-not-find-a-part-of-the-path-accessibilitymanagercompat-accessibilitystate
+             * - UWP seems not able to handle a mapped drive, like described here.
+             *   https://stackoverflow.com/questions/42020845/error-dep0700-registration-of-the-app-failed-on-windows-10-on-a-macbook-dual
+             * TODO Create one environment for both.
+             * */
+
+            IHostBuilder hostBuilder;
+
+            // Make use of own enum to switch on. Astounding it did not already exist.
+            var platform = Enum.Parse(typeof(Platform), DeviceInfo.Platform.ToString());
+
+            // Had to differentiate for platforms, as described below.
             // https://github.com/dotnet/extensions/issues/2182
+            // https://github.com/xamarin/MobileBlazorBindings/issues/41
 
-            var hostBuilder = Host.CreateDefaultBuilder().
-                ConfigureServices((context, services) =>
+            switch (platform)
+            {
+                case Platform.Android:
+                    hostBuilder = Host.CreateDefaultBuilder(); 
+                    break;
+                case Platform.iOS:
+                case Platform.UWP:
+                    hostBuilder = new HostBuilder();
+                    break;
+                default:
+                    // TODO Message.
+                    throw new Exception();
+                    break;
+            }
+
+            hostBuilder.ConfigureServices((context, services) =>
+            {
+                services.AddHttpClient();
+
+                // Note a restart is needed to actually switch IProductService,
+                // as there does not seem to be a feasible way to do that while running.
+                switch (Settings.ServiceTypeSelected)
                 {
-                    services.AddHttpClient();
+                    case Settings.ServiceType.WCF:
+                        services.AddSingleton<IProductService, WcfClient>();
+                        break;
+                    case Settings.ServiceType.WebApi:
+                        services.AddSingleton<IProductService, WebApiClient>();
+                        break;
+                }
 
-                    // Note a restart is needed to actually switch IProductService,
-                    // as there does not seem to be a feasible way to do that while running.
-                    switch (Settings.ServiceTypeSelected)
-                    {
-                        case Settings.ServiceType.WCF:
-                            services.AddSingleton<IProductService, WcfClient>();
-                            break;
-                        case Settings.ServiceType.WebApi:
-                            services.AddSingleton<IProductService,WebApiClient>();
-                            break;
-                    }
-
-                    // Note this does not work (yet) as true injection by an interface.
-                    services.AddSingleton<ProductCategoriesRepository>();
-                    services.AddSingleton<ProductSubcategoriesRepository>();
-                    services.AddSingleton<ProductsRepository>();
-                    services.AddSingleton<CartItemsRepository>();
-                });
+                // Note this does not work (yet) as true injection by an interface.
+                services.AddSingleton<ProductCategoriesRepository>();
+                services.AddSingleton<ProductSubcategoriesRepository>();
+                services.AddSingleton<ProductsRepository>();
+                services.AddSingleton<CartItemsRepository>();
+            });
 
             var host = hostBuilder.Build();
 
             ServiceProvider = host.Services;
         }
+    }
+
+    enum Platform
+    {
+        Android,
+        iOS,
+        UWP,
+        Unknown
     }
 }
