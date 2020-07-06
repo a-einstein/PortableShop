2a1352df7ccf94d3b301c88da6b35422fdc9ab16
a-einstein <aeinstein@xs4all.nl>
2020-07-06 22:35:51 +0200

    Address a Shell issue. Cleanup usings.

--- Platforms/RCS.PortableShop.UWP/App.xaml.cs
+++ Platforms/RCS.PortableShop.UWP/App.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 namespace RCS.PortableShop.UWP
@@ -57,6 +47,10 @@ namespace RCS.PortableShop.UWP
                 rootFrame = new Frame();
 
                 rootFrame.NavigationFailed += OnNavigationFailed;
+
+                // Had to add this because of:
+                // https://github.com/xamarin/Xamarin.Forms/issues/10514
+                Xamarin.Forms.Forms.SetFlags("Shell_UWP_Experimental");
 
                 Xamarin.Forms.Forms.Init(e);
 
