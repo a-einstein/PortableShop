84ad50ac83f01cf874e27ae3d33b3d88af88c239
a-einstein <aeinstein@xs4all.nl>
2021-08-19 17:36:22 +0200

    Removed comment about build/execution problems. Unclarified, might return.

--- Portable/RCS.PortableShop.ServiceClients.Products/Wrappers/ServiceClient.cs
+++ Portable/RCS.PortableShop.ServiceClients.Products/Wrappers/ServiceClient.cs
@@ -2,19 +2,6 @@
 
 namespace RCS.PortableShop.ServiceClients.Products.Wrappers
 {
-    // For some reason the libraries below are limited to 4.5.0, bound by UniversalWindowsPlatform.
-    // - System.ServiceModel.Http
-    //
-    // The following issues may be related.
-    // https://developercommunity.visualstudio.com/t/nugets-not-included-in-build/1278878
-    // https://docs.microsoft.com/en-us/answers/questions/175752/cannot-load-systemservicemodelprimitivesdll-in-uwp.html
-    // https://github.com/xamarin/Xamarin.Forms/issues/13868
-    //
-    // UWP currently only works for Release (with still a warning about System.Security.Principal.Windows).
-    // Debug fails on Microsoft.Graphics.Canvas.
-    //
-    // Solution may be no sooner than .net 6, maybe migrating to MAUI as well.
-
     public abstract class ServiceClient
     {
         // TODO Store these centrally or pass as parameter somewhere.
