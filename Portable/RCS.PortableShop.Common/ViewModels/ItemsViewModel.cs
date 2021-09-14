024ab01c4c591cc866cd27eeb419a8f03cd51fba
a-einstein <aeinstein@xs4all.nl>
2021-09-14 20:38:10 +0200

    Simplify ItemsViewModel.

--- Portable/RCS.PortableShop.Common/ViewModels/ItemsViewModel.cs
+++ Portable/RCS.PortableShop.Common/ViewModels/ItemsViewModel.cs
@@ -10,39 +10,14 @@ namespace RCS.PortableShop.Common.ViewModels
     public abstract class ItemsViewModel<TItem> : ViewModel
     {
         #region Construction
-
         protected ItemsViewModel()
         {
-            // This is still needed after initialization, it cannot be set on the ItemsProperty.
-            SetItemsCollectionChanged();
+            Items.CollectionChanged += Items_CollectionChanged;
         }
         #endregion
 
         #region Items
-        private static readonly BindableProperty ItemsProperty =
-            BindableProperty.Create(nameof(Items), typeof(ObservableCollection<TItem>), typeof(ItemsViewModel<TItem>), new ObservableCollection<TItem>());
-
-        // TODO Some sort of view would be more convenient to enable sorting in situ (filtering is no longer done so). But remember: that no longer applies when paging.
-        public ObservableCollection<TItem> Items
-        {
-            get => (ObservableCollection<TItem>)GetValue(ItemsProperty);
-
-            /*
-            TODO Preferable get rid of this kind of setters (warning CA2227). 
-            Thereby rationalizing the use of ObservableCollection versus PropertyChanged etcetera in general.
-            See other comments.
-            */
-            set
-            {
-                SetValue(ItemsProperty, value);
-                SetItemsCollectionChanged();
-            }
-        }
-
-        private void SetItemsCollectionChanged()
-        {
-            Items.CollectionChanged += Items_CollectionChanged;
-        }
+        public ObservableCollection<TItem> Items { get; } = new ObservableCollection<TItem>();
 
         protected virtual void Items_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
