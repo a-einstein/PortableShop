ef0c2b132e5a7dfe72bf9fc2640e34aa86b904ad
a-einstein <aeinstein@xs4all.nl>
2021-09-14 17:56:58 +0200

    Comments on bindings.

--- Portable/RCS.PortableShop.Common/ViewModels/ViewModel.cs
+++ Portable/RCS.PortableShop.Common/ViewModels/ViewModel.cs
@@ -7,6 +7,7 @@ using View = RCS.PortableShop.Common.Views.View;
 namespace RCS.PortableShop.Common.ViewModels
 {
     // Note that BindableObject handles PropertyChanged, but is limited to Xamarin.
+    // TODO Possibly move on to Microsoft.Toolkit.Mvvm.ComponentModel.ObservableObject.
     public abstract class ViewModel : BindableObject
     {
         #region Construction
