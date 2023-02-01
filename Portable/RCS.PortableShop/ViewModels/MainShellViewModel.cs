81b12aa659fb7714473daaf8cd074842d012f63e
a-einstein <aeinstein@xs4all.nl>
2023-02-01 15:23:38 +0100

    Update documentation-link .

--- Portable/RCS.PortableShop/ViewModels/MainShellViewModel.cs
+++ Portable/RCS.PortableShop/ViewModels/MainShellViewModel.cs
@@ -61,8 +61,8 @@ namespace RCS.PortableShop.ViewModels
 
         private static void OpenSupport()
         {
-            // TODO Make this Configureable.
-            OpenWeb("https://github.com/a-einstein/PortableShop/blob/master/README.md");
+            // TODO Make this Configurable.
+            OpenWeb("https://a-einstein.github.io/PortableShop/");
         }
         #endregion
 
@@ -83,7 +83,7 @@ namespace RCS.PortableShop.ViewModels
 
             // TODO Check versions first. Proceed with either reporting no update needed, or consent to close the application. Even better: check automatically at startup.
 
-            // TODO Make this Configureable.
+            // TODO Make this Configurable.
             OpenWeb("https://github.com/a-einstein/PortableShop/releases");
         }
         #endregion
